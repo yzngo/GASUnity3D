6551900050257011607bb1332ed023649ae55131
Yzngo <yzngo@live.com>
2020-07-07 16:33:39 +0800

    update

---
+++ Assets/Plugins/GameplayAbilitySystem/Attribute/AttributeChangeData.cs
@@ -0,0 +1,43 @@
+using GAS.GameplayEffects;
+using GAS.Interfaces;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace GAS.Attributes {
+
+    /// <summary>
+    /// Container for attribute change events
+    /// </summary>
+    [Serializable]
+    public struct AttributeChangeData {
+
+        /// <summary>
+        /// <see cref="IAttribute"/> that was change
+        /// </summary>
+        public IAttribute Attribute;
+
+        // /// <summary>
+        // /// Old value of the <see cref="IAttribute"/>
+        // /// </summary>
+        // public float OldValue;
+
+        // /// <summary>
+        // /// The <see cref="GameplayEffect"/> that caused the <see cref="IAttribute"/> change
+        // /// </summary>
+        // public GameplayEffect Effect;
+
+        // /// <summary>
+        // /// <see cref="IGameplayEffectModifier"> belonging to the <see cref="GameplayEffect"/> that caused this change.
+        // /// We can extract the <see cref="IAttribute"/> from here to see what attribute was modified.
+        // /// </summary>
+        // public IGameplayEffectModifier Modifier;
+
+        // /// <summary>
+        // /// The target of this effect
+        // /// </summary>
+        // public IGameplayAbilitySystem Target;
+
+    }
+
+}
--- Assets/Plugins/GameplayAbilitySystem/Attribute/AttributeType.cs
+++ Assets/Plugins/GameplayAbilitySystem/Attribute/AttributeType.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace GAS.Attributes {
     [CreateAssetMenu(fileName = "Attribute Type", menuName = "Ability System/Attribute Type")]
+    //相当于在editor中定义枚举值? 仅仅用于定义是哪一个类型
     public class AttributeType : ScriptableObject {
-
     }
 }
