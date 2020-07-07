d6b2b2f1815a678bb2e6de72dd71b70ea319cd04
Yzngo <yzngo@live.com>
2020-07-07 15:04:07 +0800

    update

--- Assets/Plugins/GameplayAbilitySystem/Enums/ModifierCalculationType.cs
+++ Assets/Plugins/GameplayAbilitySystem/Enums/ModifierCalculationType.cs
@@ -1,9 +1,9 @@
 namespace GameplayAbilitySystem.Enums {
     //根据不同规则产生float值给modifier使用,使其基于选定的operation改变指定的attribute
     public enum ModifierCalculationType {
-        ScalableFloat,
-        AttributeBased, 
+        ScalableFloat,  //按照表格读取技能相应等级对应的float值, 只有一个值则相当于硬编码float值
+        AttributeBased, //从attribute中读取值
         CustomCalculationClass, 
-        SetByCaller
+        SetByCaller     //运行时由caller设置
     }
 }
--- Assets/Plugins/GameplayAbilitySystem/GameplayEffectPolicy.cs
+++ Assets/Plugins/GameplayAbilitySystem/GameplayEffectPolicy.cs
@@ -5,6 +5,8 @@ using GameplayAbilitySystem.Interfaces;
 using UnityEngine;
 
 namespace GameplayAbilitySystem.GameplayEffects {
+    // Effect的策略集合
+    // - 时长策略 (立即生效, 区间+magnitude, 无穷)
     [Serializable]
     public class GameplayEffectPolicy : IGameplayEffectPolicy {
         [SerializeField]
@@ -14,11 +16,11 @@ namespace GameplayAbilitySystem.GameplayEffects {
         private float durationMagnitude = 0f;
 
         [SerializeField]
-        private List<GameplayEffectModifier> _modifiers = new List<GameplayEffectModifier>();
+        private List<GameplayEffectModifier> modifiers = new List<GameplayEffectModifier>();
 
         public DurationPolicy DurationPolicy => durationPolicy;
         public float DurationMagnitude => durationMagnitude;
-        public List<GameplayEffectModifier> Modifiers => _modifiers;
+        public List<GameplayEffectModifier> Modifiers => modifiers;
     }
 
 }
