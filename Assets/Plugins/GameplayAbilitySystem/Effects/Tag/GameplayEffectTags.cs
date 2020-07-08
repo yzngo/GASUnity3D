f6bdba967383eea9563e54c58de4b18853385698
Yzngo <yzngo@live.com>
2020-07-09 00:25:04 +0800

    update

--- Assets/Plugins/GameplayAbilitySystem/Effects/Tag/GameplayEffectTags.cs
+++ Assets/Plugins/GameplayAbilitySystem/Effects/Tag/GameplayEffectTags.cs
@@ -7,35 +7,35 @@ namespace GAS.GameplayEffects {
     [Serializable]
     public class GameplayEffectTags : IGameplayEffectTags {
         [SerializeField]
-        private GameplayEffectAddRemoveTagContainer assetTags = new GameplayEffectAddRemoveTagContainer();
+        private GameplayEffectAddRemoveTagContainer effectTags = new GameplayEffectAddRemoveTagContainer();
 
         [SerializeField]
-        private GameplayEffectAddRemoveTagContainer grantedTags = new GameplayEffectAddRemoveTagContainer();
+        private GameplayEffectAddRemoveTagContainer grantedToASCTags = new GameplayEffectAddRemoveTagContainer();
 
         [SerializeField]
-        private GameplayEffectRequireIgnoreTagContainer ongoingTagRequirements = new GameplayEffectRequireIgnoreTagContainer();
+        private GameplayEffectRequireIgnoreTagContainer ongoingRequiredTags = new GameplayEffectRequireIgnoreTagContainer();
 
         [SerializeField]
-        private GameplayEffectRequireIgnoreTagContainer applicationTagRequirements = new GameplayEffectRequireIgnoreTagContainer();
+        private GameplayEffectRequireIgnoreTagContainer applyRequiredTags = new GameplayEffectRequireIgnoreTagContainer();
 
         [SerializeField]
-        private GameplayEffectAddRemoveStacksTagContainer removeGameplayEffectsWithTag = new GameplayEffectAddRemoveStacksTagContainer();
+        private GameplayEffectAddRemoveStacksTagContainer beRemovedEffectsTags = new GameplayEffectAddRemoveStacksTagContainer();
 
         // 此Effect具有的标签
-        public GameplayEffectAddRemoveTagContainer EffectTags => assetTags;
+        public GameplayEffectAddRemoveTagContainer EffectTags => effectTags;
 
         // 赋予AbilitySystemComponent的标签
         // 移除时也会从ASC中移除, 只能用于Duration和Infinite的GameEffect
-        public GameplayEffectAddRemoveTagContainer GrantedToASCTags => grantedTags;
+        public GameplayEffectAddRemoveTagContainer GrantedToASCTags => grantedToASCTags;
         // ongoing (持续存在的)
         // 决定Modifier是开还是关  Ongoing(持续存在的), 仅作用于Duration, Infinate
-        public GameplayEffectRequireIgnoreTagContainer OngoingRequiredTags => ongoingTagRequirements;
+        public GameplayEffectRequireIgnoreTagContainer OngoingRequiredTags => ongoingRequiredTags;
 
         // 目标对象上有这些tag才可以应用
-        public GameplayEffectRequireIgnoreTagContainer ApplyRequiredTags => applicationTagRequirements;
+        public GameplayEffectRequireIgnoreTagContainer ApplyRequiredTags => applyRequiredTags;
 
         // 移除任何带有这些tag的effect
-        public GameplayEffectAddRemoveStacksTagContainer BeRemovedEffectsTags => removeGameplayEffectsWithTag;
+        public GameplayEffectAddRemoveStacksTagContainer BeRemovedEffectsTags => beRemovedEffectsTags;
 
     }
 
