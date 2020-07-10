1c31322ffcc43c154ed866d0134abe9961c057fb
Yzngo <yzngo@live.com>
2020-07-10 16:49:53 +0800

    Update EffectTagContainer.cs

--- Assets/Plugins/AbilitySystem/Effects/Tag/EffectTagContainer.cs
+++ Assets/Plugins/AbilitySystem/Effects/Tag/EffectTagContainer.cs
@@ -7,24 +7,20 @@ namespace GameplayAbilitySystem.Effects
     [Serializable]
     public class EffectTagContainer
     {
-        [Tooltip("Tags for this effect")]
+        [Tooltip("Tags for this effect.")]
         [SerializeField] private List<GameplayTag> effectTags = new List<GameplayTag>();
 
-        [Tooltip("??????????????????????")]
+        [Tooltip("Only for durational effect.")]
         [SerializeField] private List<GameplayTag> grantedToInstigatorTags = new List<GameplayTag>();
 
         [Tooltip("Effects with these Tags will removed when this effect execute.")]
         [SerializeField] private RemovedEffectTagContainer removedEffectsTags = new RemovedEffectTagContainer();
 
 
-        // 此Effect具有的标签
         public List<GameplayTag> EffectTags => effectTags;
-
         // 赋予AbilitySystemComponent的标签
         // 移除时也会从ASC中移除, 只能用于Duration和Infinite的GameEffect
         public List<GameplayTag> GrantedToInstigatorTags => grantedToInstigatorTags;
-
-        // 移除任何带有这些tag的effect
         public RemovedEffectTagContainer RemovedEffectsTags => removedEffectsTags;
 
         // [SerializeField]
