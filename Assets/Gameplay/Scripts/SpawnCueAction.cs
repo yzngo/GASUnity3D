46b841d54b64e06878827631919012d22e596636
Yzngo <yzngo@live.com>
2020-07-17 20:27:35 +0800

    update

--- Assets/Gameplay/Scripts/SpawnCueAction.cs
+++ Assets/Gameplay/Scripts/SpawnCueAction.cs
@@ -5,18 +5,19 @@ using GameplayAbilitySystem.Cues;
 using UniRx.Async;
 using UnityEngine;
 
-namespace AbilitySystemDemo {
-
+namespace AbilitySystemDemo 
+{
     [CreateAssetMenu(fileName = "Spawn Object Gameplay Cue", menuName = "Ability System Demo/Gameplay Cue/Spawn Object Gameplay Cue")]
-    class SpawnObjectGameplayCueAction : BaseCueAction {
+    class SpawnCueAction : BaseCueAction 
+    {
         public GameObject ObjectToSpawn = default;
         public Vector3 Position = default;
         public Quaternion Rotation = default;
         public Vector3 Scale = Vector3.one;
         public float DestroyInSeconds = -1;
 
-        public override async void Action(AbilitySystem target) {
-
+        public override async void Action(AbilitySystem target) 
+        {
             Time.timeScale = 0.5f;
             await UniTask.DelayFrame(5);
             Time.timeScale = 1;
