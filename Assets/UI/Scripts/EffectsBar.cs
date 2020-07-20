d9eb3a304585d7a6d8d1c57c3d66c40b11f447ba
Yzngo <yzngo@live.com>
2020-07-20 14:30:47 +0800

    update

--- Assets/UI/Scripts/EffectsBar.cs
+++ Assets/UI/Scripts/EffectsBar.cs
@@ -5,12 +5,12 @@ using GameplayAbilitySystem;
 public class EffectsBar : MonoBehaviour 
 {
     private AbilitySystem instigator;
-    private EffectTile[] GameplayTagIndicator;
+    private EffectTile[] effectTiles;
 
     private void Awake() 
     {
         instigator = GameObject.FindWithTag("Player").GetComponent<AbilitySystem>();
-        GameplayTagIndicator = GetComponentsInChildren<EffectTile>();
+        effectTiles = GetComponentsInChildren<EffectTile>();
     }
 
     void Update() 
@@ -22,27 +22,27 @@ public class EffectsBar : MonoBehaviour
 
         int tileIndex = 0;
         foreach(var effectInfo in effectsInfo) {
-            if (GameplayTagIndicator.Length < tileIndex ) return;
+            if (effectTiles.Length < tileIndex ) return;
             if (effectInfo.EffectContext.Effect.Icon == null) continue;
 
             float elapsedTime = effectInfo.EffectContext.ElapsedTime;
             float totalTime = effectInfo.EffectContext.TotalTime;
             float remainingPercent =  totalTime > 0 ? 1 - elapsedTime / totalTime : 0;
 
-            GameplayTagIndicator[tileIndex].SetRemainingPercent(remainingPercent);
-            GameplayTagIndicator[tileIndex].ImageIcon.sprite = effectInfo.EffectContext.Effect.Icon;
-            GameplayTagIndicator[tileIndex].ImageIcon.color = new Color( 1.0f, 1.0f, 1.0f);
-            GameplayTagIndicator[tileIndex].GetComponentInChildren<RectTransform>(true).gameObject.SetActive(true);
-            GameplayTagIndicator[tileIndex].SetStacks(effectInfo.Stacks);
+            effectTiles[tileIndex].SetRemainingPercent(remainingPercent);
+            effectTiles[tileIndex].ImageIcon.sprite = effectInfo.EffectContext.Effect.Icon;
+            effectTiles[tileIndex].ImageIcon.color = new Color( 1.0f, 1.0f, 1.0f);
+            effectTiles[tileIndex].GetComponentInChildren<RectTransform>(true).gameObject.SetActive(true);
+            effectTiles[tileIndex].SetStacks(effectInfo.Stacks);
             tileIndex++;
         }
 
-        for (int i = tileIndex; i < GameplayTagIndicator.Length; i++) {
-            GameplayTagIndicator[i].ImageIcon.sprite = null;
-            GameplayTagIndicator[i].ImageIcon.color = new Color(0, 0, 0, 0);
-            GameplayTagIndicator[i].SetRemainingPercent(0);
-            GameplayTagIndicator[i].GetComponentInChildren<RectTransform>(true).gameObject.SetActive(false);
-            GameplayTagIndicator[i].SetStacks(0);
+        for (int i = tileIndex; i < effectTiles.Length; i++) {
+            effectTiles[i].ImageIcon.sprite = null;
+            effectTiles[i].ImageIcon.color = new Color(0, 0, 0, 0);
+            effectTiles[i].SetRemainingPercent(0);
+            effectTiles[i].GetComponentInChildren<RectTransform>(true).gameObject.SetActive(false);
+            effectTiles[i].SetStacks(0);
         }
     }
 }
