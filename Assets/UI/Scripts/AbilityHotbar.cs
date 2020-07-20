fbfa3d8614ec30be3892fe46623781dcfa1933f0
Yzngo <yzngo@live.com>
2020-07-20 14:37:32 +0800

    Update AbilityHotbar.cs

--- Assets/UI/Scripts/AbilityHotbar.cs
+++ Assets/UI/Scripts/AbilityHotbar.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 using GameplayAbilitySystem.Abilities;
 using static GameplayAbilitySystem.Abilities.Ability;
 
-public class AbilityHotbar : MonoBehaviour {
-    public AbilityCharacter AbilityCharacter;
+public class AbilityHotbar : MonoBehaviour 
+{
+    private AbilityCharacter abilityCharacter;
     public List<BaseTile> AbilityButtons;
 
-    void Awake() {
-        for (int i = 0; i < AbilityCharacter.abilities.Count; i++) {
+    void Awake() 
+    {
+        abilityCharacter = GameObject.FindWithTag("Player").GetComponent<AbilityCharacter>();
+        for (int i = 0; i < abilityCharacter.abilities.Count; i++) {
             if (AbilityButtons.Count > i) {
-                AbilityButtons[i].ImageIcon.sprite = AbilityCharacter.abilities[i].ability.Icon;
+                AbilityButtons[i].ImageIcon.sprite = abilityCharacter.abilities[i].ability.Icon;
                 AbilityButtons[i].ImageIcon.color = new Color(1.0f, 1.0f, 1.0f); 
             }
         }
-
     }
 
-    void Update() {
+    void Update() 
+    {
         for (int i = 0; i < AbilityButtons.Count; i++) {
             var button = AbilityButtons[i];
             CoolDownInfo info = GetCooldownOfAbility(i);
@@ -30,11 +33,11 @@ public class AbilityHotbar : MonoBehaviour {
 
     public CoolDownInfo GetCooldownOfAbility(int n)
     {
-        if (n >= AbilityCharacter.abilities.Count) {
+        if (n >= abilityCharacter.abilities.Count) {
             return new CoolDownInfo();
         }
-        Ability ability = AbilityCharacter.abilities[n].ability;
-        CoolDownInfo cooldownInfo = ability.CalculateCooldown(AbilityCharacter.AbilitySystem);
+        Ability ability = abilityCharacter.abilities[n].ability;
+        CoolDownInfo cooldownInfo = ability.CalculateCooldown(abilityCharacter.AbilitySystem);
         return cooldownInfo;
     }
 }
