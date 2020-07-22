f8462a754ecfa083715b15bf796062f4e5c669f0
Yzngo <yzngo@live.com>
2020-07-22 17:03:34 +0800

    Update SpawnCueAction.cs

--- Assets/Gameplay/Scripts/AbilityAction/SpawnCueAction.cs
+++ Assets/Gameplay/Scripts/AbilityAction/SpawnCueAction.cs
@@ -23,50 +23,10 @@ namespace AbilitySystemDemo
             // await UniTask.DelayFrame(5);
             // Time.timeScale = 1;
             Transform go = Instantiate(particleToSpawn, target.transform).transform;
-            // ParticleSystem[] particleSystems = target.GetComponentsInChildren<ParticleSystem>();
-            //     bool allStopped = false;
-            //     while(!allStopped) {
-            //         if (!particleSystems[0].isStopped) {
-            //             allStopped = false;
-            //             await UniTask.DelayFrame(0);
-            //         } else {
-            //             GameObject.DestroyImmediate(go.gameObject);
-            //             break;
-            //         }
-            //     }
-            // float length = ParticleSystemLength(target.transform);
             if (destroyInSeconds > 0) {
                 await UniTask.Delay(TimeSpan.FromSeconds(destroyInSeconds));
                 GameObject.DestroyImmediate(go.gameObject);
             }
         }
-        // private float ParticleSystemLength(Transform transform)
-        // {
-        //     ParticleSystem[] particleSystems = transform.GetComponentsInChildren<ParticleSystem>();
-        //     float maxDuration = 0f;
-
-        //     foreach(var p in particleSystems) {
-        //         if(p.emission.enabled) {
-        //             if(p.main.loop) {
-        //                 return -1f;
-        //             }
-
-        //             float duration = 0f;
-        //             if(p.emission.rateOverTime.constantMax <= 0) {
-        //                 duration = p.main.startDelay.constantMax + p.main.startLifetime.constantMax;
-        //             } else {
-        //                 duration = p.main.startDelay.constantMax + Mathf.Max(p.main.duration, p.main.startLifetime.constantMax);
-        //                 // Debug.Log(p.main.startDelay.constantMax + " " + p.main.duration + " " + p.main.startLifetime.constantMax);
-        //             }
-
-        //             if (duration > maxDuration) {
-        //                 maxDuration = duration;
-        //             }
-        //         }
-        //     }
-        //     Debug.Log("max duration: " + maxDuration);
-        //     return maxDuration;
-        // }
-
     }
 }
